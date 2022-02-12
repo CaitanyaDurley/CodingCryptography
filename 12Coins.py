f502751013938be963aae7f514ab77e59b8ad952
CaitanyaDurley <caitanya.v.durley@gmail.com>
2022-02-12 21:30:03 +0000

    Modified 12Coins to allow for case when all coins are genuine

--- 12Coins.py
+++ 12Coins.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def solve(f, p):
-    m = np.size(p, 1)
+    m = np.size(p, 1) - 1
     c = np.zeros(m, int)
     max = -1
     while c is not None:
@@ -16,37 +16,33 @@ def solve(f, p):
     L = np.where(c == 1)[0]
     R = np.where(c == 2)[0]
     print(f'L: {L + 1},  R: {R + 1}')
-    p = simulate(f, N, L, R, p)
-    # input(p)
-    if np.count_nonzero(p) == 0:
-        return 'All coins are genuine'
+    p = simulate(f, N, L, R, p, m)
     if np.count_nonzero(p) == 1:
         w, f = np.argwhere(p)[0]
+        if f == m:
+            return 'All coins are genuine'
         if w == 0:
             return f'Coin {f + 1} is light'
-        else:
-            return f'Coin {f + 1} is heavy'
+        return f'Coin {f + 1} is heavy'
     return solve(f, p)
     
 
-def simulate(f, N, L, R, p):
+def simulate(f, N, L, R, p, m):
+    # Bayes => normalise non-zero probabilities
     # Balanced
-    if f[0] - 1 in N:
+    if f[0] - 1 in N or f[0] == 0:
         # Left or Right impossible
         p[0, L] = p[0, R] = p[1, L] = p[1, R] = 0
-        # Bayes => normalise non-zero probabilities
         p /= np.sum(p)
     # Left low = (left & heavy) or (right & light)
     if (f[0] - 1 in L and f[1] == 'H') or (f[0] - 1 in R and f[1] == 'L'):
-        # Left light, right heavy & neither impossible
-        p[0, L] = p[1, R] = p[0, N] = p[1, N] = 0
-        # Bayes => normalise non-zero probabilities
+        # Left light, right heavy, neither & allgenuine impossible
+        p[0, L] = p[1, R] = p[0, N] = p[1, N] = p[0, m] = 0
         p /= np.sum(p)
     # Left high = (left & light) or (right & heavy)
     if (f[0] - 1 in L and f[1] == 'L') or (f[0] - 1 in R and f[1] == 'H'):
-        # Left heavy, right light & neither impossible
-        p[1, L] = p[0, R] = p[0, N] = p[1, N] = 0
-        # Bayes => normalise non-zero probabilities
+        # Left heavy, right light, neither & allgenuine impossible
+        p[1, L] = p[0, R] = p[0, N] = p[1, N] = p[0, m] = 0
         p /= np.sum(p)
     return p
 
@@ -54,8 +50,10 @@ def simulate(f, N, L, R, p):
 def entropy(c, p):
     if np.count_nonzero(c == 1) != np.count_nonzero(c == 2):
         return 0
-    leftLow = np.sum(p[0, c == 2]) + np.sum(p[1, c == 1])
-    leftHigh = np.sum(p[0, c == 1]) + np.sum(p[1, c == 2])
+    # Remove allgenuine column so can use boolean indexing
+    q = p[:,:-1]
+    leftLow = np.sum(q[0, c == 2]) + np.sum(q[1, c == 1])
+    leftHigh = np.sum(q[0, c == 1]) + np.sum(q[1, c == 2])
     balanced = 1 - leftLow - leftHigh
     return - xlogx(leftLow) - xlogx(leftHigh) - xlogx(balanced)
 
@@ -76,7 +74,20 @@ def increment(c):
     return None
 
 
-#First row is light, second row is heavy
-m = 12
-f = (1, 'H')
-print('Forgery:', solve(f, np.full((2, m), 1 / (2*m))))
+def main():
+    m = int(input('Enter # of coins (<=12 is sensible): '))
+    f0 = int(input('Enter forged coin number (0 for all genuine): '))
+    if f0 > 0:
+        f1 = input('Enter H for heavy or L for light: ')
+    else:
+        f1 = ''
+    f = (f0, f1)
+    # First row is light, second row is heavy
+    # p[0, m] = Prob(all genuine)
+    p = np.full((2, m + 1), 1 / (2 * m + 1))
+    p[1, m] = 0
+    print(solve(f, p))
+
+
+if __name__ == '__main__':
+    main()
